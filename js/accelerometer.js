3787119bf343a5963a6eced1d4db573036b4d1c6
ryselis <djryse@gmail.com>
2013-04-30 19:05:55 +0300

    accelerometer test

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -2,13 +2,13 @@ function onDeviceMotion(event){
 	var accelerationX = event.accelerationIncludingGravity.x;
 	var accelerationY = event.accelerationIncludingGravity.y;
 	var accelerationZ = event.accelerationIncludingGravity.z;
-	if (accelerationZ > 2){
+	if (accelerationZ > 0){
 		tankMove('forward', true);
 	}
 	else{
 		tankMove('forward', false);
 	}
-	if (accelerationZ < -2){
+	if (accelerationZ < 0){
 		tankMove('back', true);
 	}
 	else{
