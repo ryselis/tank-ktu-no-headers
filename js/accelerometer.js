d1844001a1aa9c41cf3db7962313d9f933d90a2b
ryselis <djryse@gmail.com>
2013-04-30 19:38:38 +0300

    div

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -2,13 +2,20 @@ function onDeviceMotion(event){
 	var accelerationX = event.accelerationIncludingGravity.x;
 	var accelerationY = event.accelerationIncludingGravity.y;
 	var accelerationZ = event.accelerationIncludingGravity.z;
-	if (accelerationZ < - 4){
+	if (accelerationZ < -8){
 		tankMove('forward', true);
 		$("#state").text('forward true' + accelerationZ);
 	}
 	else{
 		tankMove('forward', false);
 		$("#state").text('forward false' + accelerationZ);
+	}
+	
+	if (accelerationZ > 0){
+		tankMove('back', true);
+	}
+	else{
+		tankMove('back', false);
 	}
 	/*if (accelerationZ < -2){
 		tankMove('back', true);
