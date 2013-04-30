dd2c968abd4ba5a9a062b90c136d357eddab7762
waive <waive@mail.com>
2013-04-30 17:49:57 +0300

    Just trying touchwipe.js

--- js/ready.js
+++ js/ready.js
@@ -51,4 +51,13 @@ $(document).ready(function() {
 	screenfull.onerror = function() {
 		alert("Fullscreen API error");
 	}
+	
+	$('#tankBody').touchwipe({
+	    wipeLeft: function() { alert("left"); },
+     	wipeRight: function() { alert("right"); },
+    	wipeUp: function() { alert("up"); },
+     	wipeDown: function() { alert("down"); },
+     	min_move_x: 20,
+     	min_move_y: 20,
+     	preventDefaultEvents: true
 }); 
