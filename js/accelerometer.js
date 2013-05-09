d4107d463aacc89944695327558f77814ccf0926
ryselis <djryse@gmail.com>
2013-05-09 21:24:25 +0300

    ermahgerd it werks

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -1,3 +1,7 @@
+movesForward = false;
+movesBack = false;
+movesLeft = false;
+movesRight = false;
 function onDeviceMotion(event) {
 	if (accOn) {
 		var accelerationX = event.accelerationIncludingGravity.x;
@@ -5,52 +9,64 @@ function onDeviceMotion(event) {
 		var accelerationZ = event.accelerationIncludingGravity.z;
 		if (accelerationZ < -9.5) {
 			tankMove('forward_fast', true)
+			movesForward = true;
 			//$("#stateF").text('forward_fast true');
 		} else {
 			if (accelerationZ < -8) {
 				tankMove('forward', true);
+				movesForward = true;
 				//$("#stateF").text('forward true');
-			} else {
+			} else if (movesForward) {
 				tankMove('forward', false);
+				movesForward = false;
 				//$("#stateF").text('forward false');
 			}
 		}
 		if (accelerationZ > 1.5) {
 			//$("#stateB").text('back_fast true');
 			tankMove('back_fast', true);
+			movesBack = true;
 		} else {
 			if (accelerationZ > 0) {
 				//$("#stateB").text('back true');
 				tankMove('back', true);
-			} else {
+				movesBack = true;
+			} else if (movesBack){
 				//$("#stateB").text('back false');
 				tankMove('back', false);
+				movesBack = false;
 			}
 		}
 
 		if (accelerationY > 5.5) {
 			//$('#stateL').text('left_fast true');
 			tankMove('left_fast', true);
+			movesLeft = true;
 		} else { 
 			if (accelerationY > 4) {
 				//$('#stateL').text('left true');
 				tankMove('left', true);
-			} else {
+				movesLeft = true;
+			} else if(movesLeft){
 				//$('#stateL').text('left false');
 				tankMove('left', false);
+				movesLeft = false;
 			}
 		}
 		
 		if (accelerationY < -5.5) {
 			//$('#stateR').text('right_fast true');
 			tankMove('right_fast', true);
+			movesRight = true;
 		} else {
 			if (accelerationY < -4) {
 				//$('#stateR').text('right true');
 				tankMove('right', true);
-			} else {
+				movesRight = true;
+			} else if(movesRight){
 				//$('#stateR').text('right false');
 				tankMove('right', false);
+				movesRight = false;
 			}
 		}
 	}
@@ -59,5 +75,5 @@ function onDeviceMotion(event) {
  tankMove('back', true);
  }
  else{
- tankMove('back', false);
+ tankMove('back', false);s
  }*/
