8742005a0732cf98ce3ba4df5e0845c55dcf32c3
waive <waive@mail.com>
2013-05-17 13:35:30 +0300

    configuring accelerometer values

--- js/accelerometer.js
+++ js/accelerometer.js
@@ -10,13 +10,13 @@ function onDeviceMotion(event) {
 //		$("#stateF").text('x: ' + accelerationX);
 //		$('#stateL').text('y: ' + accelerationY);
 //		$('#stateR').text('z: ' + accelerationZ);
-		if (accelerationZ < -9.5) {
+		if (accelerationZ < -6 && accelerationX > 2) {
 			tankMove('forward_fast', true)
 			movesForward = true;
 			//$("#stateF").text('forward_fast true');
 			
 		} else {
-			if (accelerationZ < -8) {
+			if (accelerationZ < -4 && accelerationX > 2) {
 				tankMove('forward', true);
 				movesForward = true;
 				//$("#stateF").text('forward true');
@@ -26,12 +26,12 @@ function onDeviceMotion(event) {
 				//$("#stateF").text('forward false');
 			}
 		}
-		if (accelerationZ > 1.5) {
+		if (accelerationZ > 2 && accelerationX < -2) {
 			//$("#stateB").text('back_fast true');
 			tankMove('back_fast', true);
 			movesBack = true;
 		} else {
-			if (accelerationZ > 0) {
+			if (accelerationZ > 0 && accelerationX < -2) {
 				//$("#stateB").text('back true');
 				tankMove('back', true);
 				movesBack = true;
@@ -42,12 +42,12 @@ function onDeviceMotion(event) {
 			}
 		}
 
-		if (accelerationY > 5.5) {
+		if (accelerationY > 4 && accelerationZ < -4) {
 			//$('#stateL').text('left_fast true');
 			tankMove('left_fast', true);
 			movesLeft = true;
 		} else { 
-			if (accelerationY > 4) {
+			if (accelerationY > 2 && accelerationZ < -4) {
 				//$('#stateL').text('left true');
 				tankMove('left', true);
 				movesLeft = true;
@@ -58,12 +58,12 @@ function onDeviceMotion(event) {
 			}
 		}
 		
-		if (accelerationY < -5.5) {
+		if (accelerationY < -4 && accelerationZ < -4) {
 			//$('#stateR').text('right_fast true');
 			tankMove('right_fast', true);
 			movesRight = true;
 		} else {
-			if (accelerationY < -4) {
+			if (accelerationY < -2 && accelerationZ < -4) {
 				//$('#stateR').text('right true');
 				tankMove('right', true);
 				movesRight = true;
